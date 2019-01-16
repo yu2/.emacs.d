ea69afc69575675a9a994c88187dc293e90467f3
yu2 <tuyu0515@gmail.com>
2019-01-16 14:39:10 -0600

    turned off evil

--- init.el
+++ init.el
@@ -1,6 +1,10 @@
 (setq debug-on-error t)
 (ido-mode 1)
 (add-to-list 'load-path (expand-file-name "elisp" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "evil" user-emacs-directory))
+(setq evil-want-C-u-scroll t)
+;;(require 'evil)
+;;(evil-mode 1)
 (require 'simpleclip)
 (simpleclip-mode 1)
 (require 'framemove)
@@ -14,15 +18,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
  '(custom-enabled-themes (quote (adwaita))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+(when (member "DejaVu Sans Mono" (font-family-list))
+  (set-face-attribute 'default nil :font "DejaVu Sans Mono"))
 
 (global-set-key (kbd "M-o") 'switch-to-previous-buffer)
 (defun switch-to-previous-buffer ()
@@ -30,3 +33,9 @@
   (switch-to-buffer (other-buffer (current-buffer) 1)))
 
 (set-terminal-coding-system 'utf-8)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
